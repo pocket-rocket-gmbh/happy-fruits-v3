73b5ed9d21c24e3e0d5b17fa0b1ce3a98d77e946
RobertFilatow <rf@pocket-rocket.io>
2023-01-03 11:52:06 +0100

    xcsdfsdf

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img alt="Vue logo" src="./assets/logo.png" />
   <h1>Welcome to our Fruit Configurator</h1>
   <div class="container">
-     <div class="columns">
-      <div class="column is-narrow">Left</div>
-      <div class="column is-8">Right</div>
+    <div class="columns">
+      <div class="column is-narrow">
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          <br />
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+          erat,<br />
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et
+          ea<br />
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem<br />
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur<br />
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+          et<br />
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+          accusam<br />
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea<br />
+          takimata sanctus est Lorem ipsum dolor sit amet.<br />
+        </p>
+      </div>
+      <div class="column is-8">
+        <p>
+          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+          <br />
+          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+          erat,<br />
+          sed diam voluptua. At vero eos et accusam et justo duo dolores et
+          ea<br />
+          rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem<br />
+          ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur<br />
+          sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+          et<br />
+          dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+          accusam<br />
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea<br />
+          takimata sanctus est Lorem ipsum dolor sit amet.<br />
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
-}
+  name: "App",
+};
 </script>
 
 <style>
